4abcb9d67f2771de46e977eeaf34792052ae5584
Nikolay <nickolay.i.tkachenko@gmail.com>
2020-08-12 10:21:31 +0300

    Initial Commit

---
+++ NetworkProjectUrl01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkProjectUrl01
+//
+//  Created by Nikolay Tkachenko on 12.08.2020.
+//  Copyright © 2020 Nikolay Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
